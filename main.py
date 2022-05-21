3656d824cdc2bd1c82d0542230ecd75348b71fc6
valmir junior <101368262+SrSouma@users.noreply.github.com>
2022-05-21 15:17:28 +0000

    ...

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from flask import Flask, flash, render_template
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret'
 
-
 @app.route('/')
 def index():
     return render_template('index1.html')
@@ -13,4 +12,5 @@ def flashing():
   flash('sucesso', 'success')
   return render_template('index.html')
 
-app.run(host='0.0.0.0', port=81)
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=8080)
